ff5964ae1aebef069b6e71c6b9b26e4c2b842ee4
AfnanSSa <iafnanz67@gmail.com>
2024-02-27 11:15:09 +0400

    Iterating through a set to print each element individually

---
+++ IterateThroughSet.java
@@ -0,0 +1,25 @@
+/* Iterate Through a Set
+* Objective: Learn to iterate over a Set and print each element.
+* Problem Statement: Create a Set containing the names of fruits
+* (e.g., "Apple", "Banana", "Cherry") and use a loop to print
+* each name individually.*/
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class SetTask {
+ public static void main(String[] args){
+     //creating set to store integers
+     Set<String> mySet = new HashSet<>();
+
+     //adding elements to the set
+     mySet.add("Apple");
+     mySet.add("Banana");
+     mySet.add("Cherry");
+
+     //iterating through the set print each element individually using enhanced for loop
+     for (String s : mySet){
+         System.out.println(s);
+     }
+ }
+}
