0be68a289db17d3536acedf1eb01435e5309f3e0
AfnanSSa <iafnanz67@gmail.com>
2024-02-27 12:09:43 +0400

    Removing all elements of set

---
+++ ClearSet.java
@@ -0,0 +1,28 @@
+/* Clear a Set
+* Objective: Learn how to remove all elements from a Set.
+* Problem Statement: Create a Set, add some elements to it,
+* and then remove all elements from the Set. Verify it's
+* empty by printing its size.*/
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+
+public class SetTask {
+ public static void main(String[] args){
+     //creating set to store integers
+     Set<Integer> integersSet = new HashSet<>();
+
+     //adding elements to the set
+     integersSet.addAll(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+     //printing th set
+     System.out.println("\nSet: " + integersSet);
+
+     //removing all elements
+     integersSet.removeAll(integersSet); //or use .clear()
+
+     //printing out the set after removing all elements 
+     System.out.println("\nSet after clear: " + integersSet);
+ }
+}
